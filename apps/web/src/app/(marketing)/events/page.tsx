1646d36cf56d3f86ed7c20aa4f4c9852c48ed49f
Keziah M. <kmuynn@gmail.com>
2026-04-11 16:01:01 -0500

    Added TODOs for event card components and update header comment

--- apps/web/src/app/(marketing)/events/page.tsx
+++ apps/web/src/app/(marketing)/events/page.tsx
@@ -1,5 +1,13 @@
 import { HeroNav } from "@/components/shared/navbar";
 
+// TODO: use suborg hero or sumn for event popup - suborg tags
+// make event card component and connect to calendar
+// make event card popup component and connect to event card
+//    links to membership portal
+//    links to stream/yt - def a way to direectly link to stream or vod
+// upcoming & past events sections
+// figure out calendar - will probably save for last..
+
 export default function EventsPage() {
     return (
         <>
@@ -9,8 +17,10 @@ export default function EventsPage() {
                         <main className="mx-auto w-full max-w-screen-xl px-10 pb-24">
 
                             <h1 className="font-chillax text-6xl font-black tracking-tight text-acm-darker-blue md:text-8xl">
-                                {/* i kind of like 'explore our events.' but open to suggestions */}
-                                see what’s happening.
+                                {/* change header text?
+                                  * i kind of like 'explore our events.' but open to suggestions
+                                */}
+                                see what's happening.
                             </h1>
             
                             <p className="mt-4 max-w-[850px] font-mono text-sm font-semibold text-acm-darker-blue/70">
